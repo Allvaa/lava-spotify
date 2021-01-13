d142cca0a536ae7a24c7ce970431e3898152d2b4
Allvaa <alvamailindo@gmail.com>
2021-01-13 11:16:36 +0700

    feat(Node): wrap all methods in Resolver into Node#load

--- dist/structures/Node.d.ts
+++ dist/structures/Node.d.ts
@@ -1,9 +1,11 @@
 import Client from "../Client";
-import { NodeOptions } from "../typings";
+import { LavalinkTrackResponse, NodeOptions } from "../typings";
 import Resolver from "./Resolver";
 export default class Node {
     client: Client;
     options: NodeOptions;
     resolver: Resolver;
+    private readonly methods;
     constructor(client: Client, options: NodeOptions);
+    load(url: string): Promise<LavalinkTrackResponse>;
 }
--- src/structures/Node.ts
+++ src/structures/Node.ts
@@ -1,9 +1,19 @@
 import Client from "../Client";
-import { NodeOptions } from "../typings";
+import { LavalinkTrackResponse, NodeOptions } from "../typings";
 import Resolver from "./Resolver";
 
 export default class Node {
     public resolver = new Resolver(this);
+    private readonly methods = {
+        album: this.resolver.getAlbum.bind(this.resolver),
+        playlist: this.resolver.getPlaylist.bind(this.resolver),
+        track: this.resolver.getTrack.bind(this.resolver)
+    };
 
     public constructor(public client: Client, public options: NodeOptions) {}
+
+    public load(url: string): Promise<LavalinkTrackResponse> {
+        const [, type, id] = this.client.spotifyPattern.exec(url) ?? [];
+        return this.methods[type as keyof Node["methods"]](id);
+    }
 }
