305c631152380e00f1034938fc71dcaef53d54ab
Allvaa <Allvaa@users.noreply.github.com>
2021-02-03 12:34:55 +0000

    chore(build): from 01ced5320727bd3541cd4156a496548770ff0eb9

--- dist/Util.d.ts
+++ dist/Util.d.ts
@@ -1,4 +1,5 @@
 export default class Util {
     static try<T>(fn: () => T): T | undefined;
     static tryPromise<T>(fn: () => Promise<T>): Promise<T | undefined>;
+    static structuredClone<T>(obj: T): T;
 }
