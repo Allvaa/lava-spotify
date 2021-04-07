33e6d803aa3c122bf0a8cbafed800e6a84fcb1a7
Allvaa <Allvaa@users.noreply.github.com>
2021-04-07 16:26:12 +0000

    chore(build): from d48db4ca1d19d4463730c6b88e60b21ed4139396

--- dist/Constants.js
+++ dist/Constants.js
@@ -5,6 +5,7 @@ exports.DefaultClientOptions = {
     clientID: "",
     clientSecret: "",
     playlistPageLimit: 2,
-    audioOnlyResults: false
+    audioOnlyResults: false,
+    useSpotifyMetadata: false
 };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQ29uc3RhbnRzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL0NvbnN0YW50cy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFFYSxRQUFBLG9CQUFvQixHQUFrQjtJQUMvQyxRQUFRLEVBQUUsRUFBRTtJQUNaLFlBQVksRUFBRSxFQUFFO0lBQ2hCLGlCQUFpQixFQUFFLENBQUM7SUFDcEIsZ0JBQWdCLEVBQUUsS0FBSztDQUMxQixDQUFDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQ29uc3RhbnRzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL0NvbnN0YW50cy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFFYSxRQUFBLG9CQUFvQixHQUFrQjtJQUMvQyxRQUFRLEVBQUUsRUFBRTtJQUNaLFlBQVksRUFBRSxFQUFFO0lBQ2hCLGlCQUFpQixFQUFFLENBQUM7SUFDcEIsZ0JBQWdCLEVBQUUsS0FBSztJQUN2QixrQkFBa0IsRUFBRSxLQUFLO0NBQzVCLENBQUMifQ==
