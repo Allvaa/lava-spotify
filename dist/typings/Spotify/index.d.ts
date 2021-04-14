704ddf28964f4fcdad0127079875cee3c4775abc
Allvaa <52526068+Allvaa@users.noreply.github.com>
2021-04-14 21:36:45 +0700

    fix: loading tracks when audioOnlyResults enabled returns no matches (#9)

--- dist/typings/Spotify/index.d.ts
+++ dist/typings/Spotify/index.d.ts
@@ -22,6 +22,7 @@ export interface SpotifyPlaylist {
 }
 export interface SpotifyTrack {
     artists: SpotifyArtist[];
+    duration_ms: number;
     external_urls: {
         spotify: string;
     };
