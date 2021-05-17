c70acb62bbc75b0d23dcbbf625e6cb97aced96c0
Allvaa <Allvaa@users.noreply.github.com>
2021-05-17 12:38:36 +0000

    chore(build): from ee819028cf2547046dbff76e3fd05638ce50aed6

--- dist/Constants.d.ts
+++ dist/Constants.d.ts
@@ -1,2 +1,3 @@
-import { ClientOptions } from "./typings";
+import { ClientOptions, NodeOptions } from "./typings";
 export declare const DefaultClientOptions: ClientOptions;
+export declare const DefaultNodeOptions: NodeOptions;
